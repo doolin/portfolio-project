05e4f608627bad4f895ae813565fa0f6ee177fcd
Dave Doolin <david.doolin@gmail.com>
2010-12-31 15:24:39 -0800

    Trying to configure sass.

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 PortfolioProject::Application.initialize!
 
+Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'public/stylesheets' }
