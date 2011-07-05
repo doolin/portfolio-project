bcff35bdd517c93c29f220536352b26935930c12
Dave Doolin <david.doolin@gmail.com>
2011-07-05 10:39:52 -0700

    Added configuration for email password recovery.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,8 +20,21 @@ PortfolioProject::Application.configure do
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
-  # From Devise
+ # From Devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => :plain,
+    :domain => ENV['GMAIL_SMTP_USER'],
+    :user_name => ENV['GMAIL_SMTP_USER'],
+    :password => ENV['GMAIL_SMTP_PASSWORD'],
+  }
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
