80bc5a23d3b4d3b7bee763071f9f5fb737ec276f
Dave Doolin <david.doolin@gmail.com>
2011-08-29 14:18:45 -0700

    Fixed have_selector

--- spec/views/projects/index.html.erb_spec.rb
+++ spec/views/projects/index.html.erb_spec.rb
@@ -18,9 +18,9 @@ describe "projects/index.html.erb" do
       render
       # http://groups.google.com/group/ruby-capybara/browse_thread/thread/944871152dae0272/9ca6dcaf173dab74
       # http://groups.google.com/group/ruby-capybara/msg/9ca6dcaf173dab74
-      rendered.should have_selector('tr>th', :content => 'Project name')
-      rendered.should have_selector('tr>th', :content => 'Summary')
-      rendered.should have_selector('tr>th', :content => 'Tags')
+      rendered.should have_selector('th', :content => 'Name')
+      rendered.should have_selector('th', :content => 'Summary')
+      rendered.should have_selector('th', :content => 'Tags')
     end
      
   end
