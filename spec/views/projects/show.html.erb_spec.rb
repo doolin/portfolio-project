dcda16274d23ba9a90db33707b74ab38e96dfb8d
Dave Doolin <david.doolin@gmail.com>
2011-09-11 16:50:23 -0700

    Rewrote project view specs.

--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
-describe "projects/show.html.erb" do
+describe 'projects/show.html.erb' do
 
   before(:each) do
-    @member = Factory(:member)
-    @project = Factory(:project, :member => @member)
-    sign_in @member
+    # p. 333 Lightbulb
+    assign(:project, mock_model(Project).as_new_record.as_null_object)
   end
+
+  it "infers the controller path" do
+    controller.request.path_parameters["controller"].should eq("projects")
+  end
+
+  it "infers the controller action" do
+    controller.request.path_parameters["action"].should eq("show")
+  end
+
 
   it "renders attributes in <p>" do
     render
@@ -23,6 +31,11 @@ describe "projects/show.html.erb" do
   it "has a URI field for the project" do
     render
     rendered.should have_selector('div.uri')
+  end
+
+  it "does not have a foobar field for the project" do
+    render
+    rendered.should_not have_selector('div.foobar')
   end
 
 end
