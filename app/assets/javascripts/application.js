ee65bc7be0b55efaf0c2f8657620551a39bff2d2
Dave Doolin <david.doolin@gmail.com>
2011-10-15 15:19:43 -0700

    Squaring away teh assets pipeline.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
    	
 $(document).ready( function() {
   $(".tags").css("color","black");
