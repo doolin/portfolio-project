4ebfb79e09e674dac5594e41161a10798030e216
Dave Doolin <david.doolin@gmail.com>
2011-10-28 09:13:27 -0700

    SEO title to members/show

--- spec/views/members/show.html.erb_spec.rb
+++ spec/views/members/show.html.erb_spec.rb
@@ -31,11 +31,15 @@ describe 'members/show.html.erb' do
     end
   end
 
-  xit "should have the correct <title> element " do
+  it "should have the correct <title> element " do
+    @member.firstname = 'Foo'
+    @member.lastname = 'Bar'
     render(:template => "members/show.html.erb", :layout => 'layouts/application')
     # have_selector is from webrat 
     # https://github.com/brynary/webrat/blob/master/lib/webrat/core/matchers/have_selector.rb
-    rendered.should have_selector("title", :content => "Privacy | Portfolio Project")
+    title = "#{@member.firstname} #{@member.lastname} | Portfolio Project"
+    #rendered.should have_selector("title", :content => "Dave Doolin | Portfolio Project")
+    rendered.should have_selector("title", :content => title)
     # http://blog.carbonfive.com/2011/03/02/a-look-at-specifying-views-in-rspec/
     #view.content_for(:sidebar).should have_selector('div.quote')
   end
