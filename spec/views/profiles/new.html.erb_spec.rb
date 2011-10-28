8e5d06abb0c718b17f02386aecbf9b3ee635e4dc
Dave Doolin <david.doolin@gmail.com>
2011-10-28 09:24:16 -0700

    SEO title for profile/new

--- spec/views/profiles/new.html.erb_spec.rb
+++ spec/views/profiles/new.html.erb_spec.rb
@@ -29,11 +29,11 @@ describe "profiles/new.html.erb" do
     rendered.should =~ /Firstname/
   end
 
-  xit "should have the correct <title> element " do
+  it "should have the correct <title> element " do
     render(:template => "profiles/new.html.erb", :layout => 'layouts/application')
     # have_selector is from webrat 
     # https://github.com/brynary/webrat/blob/master/lib/webrat/core/matchers/have_selector.rb
-    rendered.should have_selector("title", :content => "Privacy | Portfolio Project")
+    rendered.should have_selector("title", :content => "Create your Profile | Portfolio Project")
     # http://blog.carbonfive.com/2011/03/02/a-look-at-specifying-views-in-rspec/
     #view.content_for(:sidebar).should have_selector('div.quote')
   end
