5f3c9af087d4880d407747fe418b101bb5d82fd2
Dave Doolin <david.doolin@gmail.com>
2011-10-31 09:22:10 -0700

    Redesigned project layout on project/index

--- spec/views/projects/index.html.erb_spec.rb
+++ spec/views/projects/index.html.erb_spec.rb
@@ -18,9 +18,8 @@ describe "projects/index.html.erb" do
       render
       # http://groups.google.com/group/ruby-capybara/browse_thread/thread/944871152dae0272/9ca6dcaf173dab74
       # http://groups.google.com/group/ruby-capybara/msg/9ca6dcaf173dab74
-      rendered.should have_selector('th', :content => 'Name')
-      rendered.should have_selector('th', :content => 'Summary')
-      rendered.should have_selector('th', :content => 'Tags')
+      rendered.should have_selector('th', :content => 'Projects')
+      rendered.should have_selector('th', :content => 'Owner')
     end
      
   end
