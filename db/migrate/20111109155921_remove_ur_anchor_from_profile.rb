1d6ec627ec75316816892cf550c7f0bb0e192921
Dave Doolin <david.doolin@gmail.com>
2011-11-09 08:08:46 -0800

    cleaning from a bad migration

--- db/migrate/20111109155921_remove_ur_anchor_from_profile.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUrAnchorFromProfile < ActiveRecord::Migration
-  def up
-    remove_column :profiles, :ur_anchor
-  end
-
-  def down
-    add_column :profiles, :ur_anchor, :string
-  end
-end
