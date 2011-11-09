77738d05ca6edc5327d7af85ea91338587a5f1f9
Dave Doolin <david.doolin@gmail.com>
2011-11-09 08:00:33 -0800

    cleaning up after typo in previous migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111109155155) do
+ActiveRecord::Schema.define(:version => 20111109155921) do
 
   create_table "members", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
