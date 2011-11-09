1ad57b85c5c897662a6201117da58ac80fda0dbe
Dave Doolin <david.doolin@gmail.com>
2011-11-09 08:19:30 -0800

    removing extra migration

--- db/migrate/20111109160641_remove_url_anchor_from_profile.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUrlAnchorFromProfile < ActiveRecord::Migration
-  def up
-    remove_column :profiles, :url_anchor
-  end
-
-  def down
-    add_column :profiles, :url_anchor, :string
-  end
-end
