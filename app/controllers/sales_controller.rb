20ef4effc73bb7379c396a5c01f140a3d73d2021
Dave Doolin <david.doolin@gmail.com>
2011-12-26 09:09:11 -0800

    Started code for rendering specific gravatars on front page (which could be used anywhere).

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,6 +2,7 @@ class SalesController < ApplicationController
 
   def index
     @projects = Project.find(:all)
+    @profiles = Profile.find(:all)
   end
   
   # Add more pages as necessary, but only when necessary.
