c1e8ac2895f4afc5f916a9a75ba0d1f970803dda
Dave Doolin <david.doolin@gmail.com>
2011-12-29 10:08:54 -0800

    Added configurable profile display for front page.

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 PortfolioProject::Application.initialize!
 
+PROFILES = YAML.load_file("#{Rails.root.to_s}/config/featuredprofiles.yml")[Rails.env]
