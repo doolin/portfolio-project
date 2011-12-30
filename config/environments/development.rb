581a14ea0fbf8fb9fc4bb73066c3f790b32ef8d5
Dave Doolin <david.doolin@gmail.com>
2011-12-30 09:42:29 -0800

    Use raw files for development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,6 @@ PortfolioProject::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = false
+  config.assets.debug = true
 end
 
