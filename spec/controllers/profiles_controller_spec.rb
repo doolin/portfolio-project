ad7d814cce390ea803d98155320ee241fbc7d7bc
Dave Doolin <david.doolin@gmail.com>
2015-04-05 07:19:01 -0700

    updated profiles controller spec

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -1,17 +1,16 @@
 require 'spec_helper'
 
 describe ProfilesController do 
-  
   before(:each) do
-    @member  = Factory(:member)
-    @profile = Factory(:profile, :member => @member)
+    @member  = FactoryGirl.create(:member)
+    @profile = FactoryGirl.create(:profile, :member => @member)
     sign_in @member
   end
   
   describe "GET 'index'" do
     it "lists member profiles when signed in" do
       get :index
-      response.should render_template("index")
+      expect(response).to render_template("index")
     end
 
     it "redirects to sign in path when signed out" do
@@ -19,7 +18,7 @@ describe ProfilesController do
       get :index
       # Changing the behavior, we want the profiles page indexed for SEO.
       #response.should redirect_to(new_member_session_path)
-      response.should render_template("index")
+      expect(response).to render_template("index")
     end
   end
 
@@ -27,33 +26,31 @@ describe ProfilesController do
     it "should be successful" do
       sign_out @member
       get :show, :id => @profile.url
-      response.should be_success
+      expect(response).to be_success
     end
 
     it "displays the member's profile" do
       sign_out @member
       get :show, :id => @profile.url
-      response.should render_template("show")
+      expect(response).to render_template("show")
     end
   end
 
   describe "GET 'new'" do
     it "renders the new profile page" do
       get :new
-      response.should render_template("new")       
+      expect(response).to render_template("new")       
     end
   end
 
   describe "GET 'edit'" do
     it "renders the edit profile page" do
       get :edit, :id => @profile.url
-      response.should render_template("edit")       
+      expect(response).to render_template("edit")       
     end
   end
   
   describe "POST 'create'" do
-
-
     # TODO: This test used to pass, but apparently, if the
     # has_one association (Member -> Profile) is already
     # instantiated, this test as originally written fails.
@@ -61,69 +58,64 @@ describe ProfilesController do
     # then rebuilding it here with the :create action
     it "should create a new profile for signed in member" do
       sign_out @member
-      @newmember  = Factory(:member, :email => 'foofppf@gmail.com')
+      @newmember  = FactoryGirl.create(:member, :email => 'foofppf@gmail.com')
       sign_in @newmember
-      lambda do
+      expect do
         post :create, :profile => { :firstname => 'foo', :lastname => 'bar' }
-      end.should change(Profile, :count).by(1)
+      end.to change(Profile, :count).by(1)
     end
 
     # TODO: from note above, a little testing.
     it "should create a profile for member without a profile" do
       @member.profile.destroy
-      lambda do
+      expect do
         post :create, :profile => { :firstname => 'foo', :lastname => 'bar' }
-      end.should change(Profile, :count).by(1)
+      end.to change(Profile, :count).by(1)
     end
 
     xit "creates a new profile" do
       Profile.should_receive(:new).with('firstname' => 'foo')
       post :create, :profile => { 'firstname' => 'foo' }
     end
-    it "saves the profile"
 
+    it "saves the profile"
   end
 
   describe "PUT 'update'" do
-
     it "should update the profile for signed in member" do
       put :update, :id => @profile.url, :profile => { :firstname => 'Foo', :lastname => 'Bar' }
       @profile.reload
-      response.should redirect_to(profile_path(@profile))
-      @profile.firstname.should eq('Foo')
-      @profile.lastname.should eq('Bar')
+      expect(response).to redirect_to(profile_path(@profile))
+      expect(@profile.firstname).to eq('Foo')
+      expect(@profile.lastname).to eq('Bar')
     end
 
     it "should update the profile for nil website" do
       put :update, :id => @profile.url, :profile => { :firstname => 'Foo', :lastname => 'Bar', :website => nil }
       @profile.reload
-      response.should redirect_to(profile_path(@profile))
-      @profile.website.should be_nil
+      expect(response).to redirect_to(profile_path(@profile))
+      expect(@profile.website).to be_nil
     end
 
   end
 
   describe "DELETE 'destroy'" do
-    
     it "should destroy the profile" do
-      lambda do
+      expect do
         delete :destroy, :id => @profile.url
-      end.should change(Profile, :count).by(-1)
+      end.to change(Profile, :count).by(-1)
     end
 
     it "redirects to the projects list" do
       delete :destroy, :id => @profile.url
-      response.should redirect_to(root_path)
+      expect(response).to redirect_to(root_path)
     end
 
-=begin
-# Leave this in for a bit, the should_receive is a mock_method
-    it "destroys the requested profile" do
+    # Leave this in for a bit, the should_receive is a mock_method
+    xit "destroys the requested profile" do
       #Project.should_receive(:find).with("37") { mock_project }
       @profile.should_receive(:destroy)
       delete :destroy, :id => @profile.url
     end
-=end
   end
-
 end
