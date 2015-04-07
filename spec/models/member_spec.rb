c00be293eb824ae63d4e8f9d962533850e004253
Dave Doolin <david.doolin@gmail.com>
2015-04-07 05:01:14 -0700

    updated member spec to rspec 3

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,67 +1,57 @@
 require 'spec_helper'
 
-
 describe Member do
-
   describe "project associations" do
-
     before(:each) do
-      @member = Factory(:member)
-      @p1 = Factory(:project, :member => @member, :created_at => 1.day.ago)
-      @p2 = Factory(:project, :member => @member, :created_at => 1.hour.ago)
+      @member = FactoryGirl.create(:member)
+      @p1 = FactoryGirl.create(:project, :member => @member, :created_at => 1.day.ago)
+      @p2 = FactoryGirl.create(:project, :member => @member, :created_at => 1.hour.ago)
     end
 
     it "should have a projects attribute" do
-      @member.should respond_to(:projects)
+      expect(@member).to respond_to(:projects)
     end
 
     it "should have the right projects in the right order" do
-      @member.projects.should == [@p2, @p1]
+      expect(@member.projects).to eq [@p2, @p1]
     end
 
     it "should destroy associated projects" do
       @member.destroy
       [@p1, @p2].each do |p|
-        Project.find_by_id(p.id).should be_nil
+        expect(Project.find_by_id(p.id)).to be_nil
       end
     end
   end
 
-
   describe "profile associations" do
-
     before(:each) do
-      @member = Factory(:member)
-      @p1 = Factory(:profile, :member => @member, :created_at => 1.day.ago)
+      @member = FactoryGirl.create(:member)
+      @p1 = FactoryGirl.create(:profile, :member => @member, :created_at => 1.day.ago)
     end
 
     it "should have a profile" do
-      @member.profile.should == @p1
+      expect(@member.profile).to eq @p1
     end
 
     it "destroys the associated profile" do
       @member.destroy
       [@p1].each do |p|
-        Profile.find_by_id(p.id).should be_nil
+        expect(Profile.find_by_id(p.id)).to be_nil
       end
     end
-
   end
 
-
   describe "saving" do
-
-  	it 'requires first and last name' do
-      Factory(:member).should be_valid
-      Factory.build(:member, :firstname => nil).should_not be_valid
-      Factory.build(:member, :lastname => nil).should_not be_valid
+    it 'requires first and last name' do
+      expect(FactoryGirl.create(:member)).to be_valid
+      expect(FactoryGirl.build(:member, :firstname => nil)).not_to be_valid
+      expect(FactoryGirl.build(:member, :lastname => nil)).not_to be_valid
     end
 
     it "should enforce unique email" do
-    	m1 = Factory(:member)
-    	m2 = Factory.build(:member, :email => m1.email).should_not be_valid
+      m1 = FactoryGirl.create(:member)
+      expect(FactoryGirl.build(:member, :email => m1.email)).not_to be_valid
     end
-
   end
-
 end
