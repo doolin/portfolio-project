aa0832b791b95440c212ad715b73c9434a8189b1
Dave Doolin <david.doolin@gmail.com>
2015-04-13 05:20:12 -0700

    fixed profile/destroy view

--- spec/views/profiles/destroy.html.erb_spec.rb
+++ spec/views/profiles/destroy.html.erb_spec.rb
@@ -2,18 +2,16 @@
 require 'spec_helper'
 
 describe "profiles/destroy.html.erb" do
-
   it "infers the controller path" do
-    controller.request.path_parameters["controller"].should eq("profiles")
+    expect(controller.request.path_parameters[:controller]).to eq("profiles")
   end
-  
+
   it "infers the controller action" do
-    controller.request.path_parameters["action"].should eq("destroy")
+    expect(controller.request.path_parameters[:action]).to eq("destroy")
   end
 
   it "renders the profile destroyed page" do
     render
-    rendered.should have_selector('h2', :content => 'Thank you for your time')
+    expect(rendered).to have_selector('h2', :text => 'Thank you for your time')
   end
-
 end
