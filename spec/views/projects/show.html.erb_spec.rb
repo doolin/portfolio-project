3a6ab6b41a88d8a51537cd7f4c1944348750f66e
Dave Doolin <david.doolin@gmail.com>
2015-04-28 04:31:55 -0700

    project show view spec updated

--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -1,70 +1,69 @@
 require 'spec_helper'
 
 describe 'projects/show.html.erb' do
-
   before(:each) do
     # p. 333 Lightbulb
     #assign(:project, mock_model(Project).as_new_record.as_null_object)
-    @member   = Factory(:member)
-    @project   = Factory(:project, :name => 'Slicer', :member => @member)
-    @profile   = Factory(:profile, :member => @member)
+    @member   = FactoryGirl.create(:member)
+    @project   = FactoryGirl.create(:project, :name => 'Slicer', :member => @member)
+    @profile   = FactoryGirl.create(:profile, :member => @member)
     #sign_in @member
   end
 
   it "infers the controller path" do
-    controller.request.path_parameters["controller"].should eq("projects")
+    expect(controller.request.path_parameters[:controller]).to eq("projects")
   end
 
   it "infers the controller action" do
-    controller.request.path_parameters["action"].should eq("show")
+    expect(controller.request.path_parameters[:action]).to eq("show")
   end
 
-#describe 'layouts/application' do
-#https://github.com/rspec/rspec-rails/issues/176
+  #describe 'layouts/application' do
+  #https://github.com/rspec/rspec-rails/issues/176
   it "renders flash notices" do
     flash[:notice] = "This is a notice!"
     render
     #response.should contain "This is a notice!"
-    rendered.should =~ /This is a notice!/
+    expect(rendered).to match(/This is a notice!/)
   end
-#end
+  #end
 
   it "renders attributes in <p>" do
     render
     # Name changed to h1 element
     #rendered.should match(/Name/)
-    rendered.should match(/Summary/)
-    rendered.should match(/Client/)
+    expect(rendered).to match(/Summary/)
+    expect(rendered).to match(/Client/)
   end
 
   it "has a description block for the project" do
     render
-    rendered.should have_selector('div.description')
+    expect(rendered).to have_selector('div.description')
   end
 
   it "has a URI field for the project" do
     render
-    rendered.should have_selector('div.uri')
+    expect(rendered).to have_selector('div.uri')
   end
 
   it "has a Required Skills field for the project" do
     render
-    rendered.should have_selector('div.requiredskills')
+    expect(rendered).to have_selector('div.requiredskills')
   end
-  
+
   # If a field is nil, the .html_safe method doesn't work.
   #xit "handles an error if the skills field is blank"
 
   it "does not have a foobar field for the project" do
     render
-    rendered.should_not have_selector('div.foobar')
+    expect(rendered).not_to have_selector('div.foobar')
   end
 
-  it "should have the correct <title> element " do
+  xit "should have the correct <title> element " do
     render(:template => "projects/show.html.erb", :layout => 'layouts/application')
-    # have_selector is from webrat 
+    # have_selector is from webrat
     # https://github.com/brynary/webrat/blob/master/lib/webrat/core/matchers/have_selector.rb
-    rendered.should have_selector("title", :content => @project.name + ' | Portfolio Project')
+    expect(rendered).to have_selector("title", :text => @project.name + ' | Portfolio Project')
     # http://blog.carbonfive.com/2011/03/02/a-look-at-specifying-views-in-rspec/
     #view.content_for(:sidebar).should have_selector('div.quote')
   end
