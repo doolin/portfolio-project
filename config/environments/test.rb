0213ca16eb2382d2f9f38b1edde791199fce6475
Dave Doolin <david.doolin@gmail.com>
2015-05-16 06:17:32 -0700

    serve static files instead of static assets

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,7 +33,7 @@ PortfolioProject::Application.configure do
   config.active_support.deprecation = :stderr
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=3600"
   # This config option was shown in the episode but is actually not used, so don't bother adding it.
   config.assets.allow_debugging = true
