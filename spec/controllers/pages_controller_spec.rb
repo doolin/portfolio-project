1f041c83af304488ef7c09e982eb512149091534
Dave Doolin <david.doolin@gmail.com>
2015-05-16 06:16:22 -0700

    update expectation syntax

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,54 +1,52 @@
 require 'spec_helper'
 
 describe PagesController do
-
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
-      response.should be_success
+      expect(response).to be_success
     end
   end
 
   describe "GET 'disclaimer'" do
     it "should be successful" do
       get 'disclaimer'
-      response.should be_success
+      expect(response).to be_success
     end
   end
 
   describe "GET 'disclosure'" do
     it "should be successful" do
       get 'disclosure'
-      response.should be_success
+      expect(response).to be_success
     end
   end
 
   describe "GET 'terms'" do
     it "should be successful" do
       get 'terms'
-      response.should be_success
+      expect(response).to be_success
     end
   end
 
   describe "GET 'confirmation'" do
     it "should be successful" do
       get 'confirmation'
-      response.should be_success
+      expect(response).to be_success
     end
   end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
-      response.should be_success
+      expect(response).to be_success
     end
   end
 
   describe "GET 'privacy'" do
     it "should be successful" do
       get 'privacy'
-      response.should be_success
+      expect(response).to be_success
     end
   end
-
 end
