ab5bae285b3bc44f709c3b08e5337fb282dedcd3
Dave Doolin <david.doolin@gmail.com>
2015-05-17 06:44:49 -0700

    update expectations for members controller

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,48 +1,31 @@
 require 'spec_helper'
 
 describe MembersController do
-  
-  # Including this produces an error, probably
-  # conflicts before(:each) below, or something
-  # like that.
-  #login_admin 
-  
-  #render_views
-
   before(:each) do
     @member = FactoryGirl.create :member
   end
-  
+
   describe "GET 'show'" do
     it "should show the member's projects'" do
-      # TODO: these should move to view specs
       p1 = FactoryGirl.create(:project, :member => @member, :summary => "Foo bar")
       p2 = FactoryGirl.create(:project, :member => @member, :summary => "Baz quux")
       @member.projects = [p1, p2]
       get :show, :id => @member
-      response.should render_template("show")
+      expect(response).to render_template("show")
     end
   end
 
   describe "GET 'index'" do
-    
     it "signed in should list member profiles" do
       sign_in @member
       get :index
-      response.should redirect_to(profiles_path)
+      expect(response).to redirect_to(profiles_path)
     end
 
     it "signed out should redirect to sign in path" do
       sign_out @member
       get :index
-      response.should redirect_to(new_member_session_path)
+      expect(response).to redirect_to(new_member_session_path)
     end
-    
   end
-  
-  
-    describe "PUT update" do
-      # Devise handles this stuff
-    end
-
 end
