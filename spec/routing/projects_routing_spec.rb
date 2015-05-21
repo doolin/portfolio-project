5f60d6e594710499824ce35e4772dd8c64199eb4
Dave Doolin <david.doolin@gmail.com>
2015-05-21 06:20:53 -0700

    project routing spec expectations updated

--- spec/routing/projects_routing_spec.rb
+++ spec/routing/projects_routing_spec.rb
@@ -1,81 +1,74 @@
 require "spec_helper"
 
 describe ProjectsController do
-
   describe "routing, explicit, long form" do
-
     it "recognizes and generates #index" do
-      { :get => "/projects" }.should route_to(:controller => "projects", :action => "index")
+      expect(:get => "/projects").to route_to(:controller => "projects", :action => "index")
     end
 
     it "recognizes and generates #new" do
-      { :get => "/projects/new" }.should route_to(:controller => "projects", :action => "new")
+      expect(:get => "/projects/new").to route_to(:controller => "projects", :action => "new")
     end
 
     it "recognizes and generates #show" do
-      { :get => "/projects/1" }.should route_to(:controller => "projects", :action => "show", :id => "1")
+      expect(:get => "/projects/1").to route_to(:controller => "projects", :action => "show", :id => "1")
     end
 
     it "recognizes and generates #edit" do
-      { :get => "/projects/1/edit" }.should route_to(:controller => "projects", :action => "edit", :id => "1")
+      expect(:get => "/projects/1/edit").to route_to(:controller => "projects", :action => "edit", :id => "1")
     end
 
     it "recognizes and generates #create" do
-      { :post => "/projects" }.should route_to(:controller => "projects", :action => "create")
+      expect(:post => "/projects").to route_to(:controller => "projects", :action => "create")
     end
 
     it "recognizes and generates #update" do
-      { :put => "/projects/1" }.should route_to(:controller => "projects", :action => "update", :id => "1")
+      expect(:put => "/projects/1").to route_to(:controller => "projects", :action => "update", :id => "1")
     end
 
     it "recognizes and generates #destroy" do
-      { :delete => "/projects/1" }.should route_to(:controller => "projects", :action => "destroy", :id => "1")
+      expect(:delete => "/projects/1").to route_to(:controller => "projects", :action => "destroy", :id => "1")
     end
-
   end
 
-
   describe "routing short form, url redirect" do
-
     before(:each) do
       @project = Project.create(:name => 'New project')
     end
 
     it "routes GET projects_path to the /index action" do
-      get(projects_path).should \
+      expect(get(projects_path)).to \
         route_to('projects#index')
     end
 
     it "routes GET project_path(@project) to the /show action" do
-      get(project_path(@project)).should \
+      expect(get(project_path(@project))).to \
         route_to('projects#show', :id => @project.url)
     end
 
     it "routes GET edit_project_path(@project) to the /edit action" do
-      get(edit_project_path(@project)).should \
+      expect(get(edit_project_path(@project))).to \
         route_to('projects#edit', :id => @project.url)
     end
 
     it "routes GET new_project_path to the /new action" do
-      get(new_project_path).should \
+      expect(get(new_project_path)).to \
         route_to('projects#new')
     end
 
     it "routes POST project_path to the /create action" do
-      post(projects_path).should \
+      expect(post(projects_path)).to \
         route_to('projects#create')
     end
 
     it "routes DELETE project_path(@project) to the /destroy action" do
-      delete(project_path(@project)).should \
+      expect(delete(project_path(@project))).to \
         route_to('projects#destroy', :id => @project.url)
     end
 
     it "routes PUT project_path(@project) to the /update action" do
-      put(project_path(@project)).should \
+      expect(put(project_path(@project))).to \
         route_to('projects#update', :id => @project.url)
     end
-
   end
-
 end
