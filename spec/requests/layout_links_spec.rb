4f9ed90f075d7fecf51bde57bb2d32f606e0b919
Dave Doolin <david.doolin@gmail.com>
2015-05-26 06:19:35 -0700

    ayout links spec

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,43 +1,38 @@
-
 require 'spec_helper'
 
 describe "LayoutLinks" do
-
   it "should have a Contact page at '/contact'" do
     get '/contact'
-    response.should render_template(:contact)
+    expect(response).to render_template(:contact)
   end
 
   it "should have an About page at '/about'" do
     get '/about'
-    response.should render_template(:about)
+    expect(response).to render_template(:about)
   end
 
   it "should have a Disclaimer page at '/disclaimer'" do
-    #pending "Add this link later"
     get '/disclaimer'
-    response.should render_template(:disclaimer)
+    expect(response).to render_template(:disclaimer)
   end
 
   it "should have a Disclosure page at '/disclosure'" do
-    #pending "Add this link later"
     get '/disclosure'
-    response.should render_template(:disclosure)
+    expect(response).to render_template(:disclosure)
   end
 
   it "should have a Privacy page at '/privacy'" do
     get '/privacy'
-    response.should render_template(:privacy)
+    expect(response).to render_template(:privacy)
   end
 
   it "should have a Terms & Conditions page at '/terms'" do
     get '/terms'
-    response.should render_template(:terms)
+    expect(response).to render_template(:terms)
   end
 
   it "should have a Help page at '/help'" do
-    #pending "Develop a help system"
     get '/help'
-    response.should render_template(:help)
+    expect(response).to render_template(:help)
   end
 end
