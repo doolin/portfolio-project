1578be0bd3829cb552710fb855ed5b8c620b7c6b
Dave Doolin <david.doolin@gmail.com>
2015-05-31 07:01:40 -0700

    Clean up a view spec, still having issues with how rspec 3.2 handles db access in view specs.

--- spec/views/sales/index.html.erb_spec.rb
+++ spec/views/sales/index.html.erb_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe "sales/index.html.erb" do
+describe "sales/index" do
   before(:each) do
-    @member   = FactoryGirl.create(:member)
-    @project   = FactoryGirl.create(:project, :name => 'Slicer', :member => @member)
-    @profile   = FactoryGirl.create(:profile, :member => @member)
+    @member  = FactoryGirl.create(:member)
+    @project = FactoryGirl.create(:project, :name => 'Slicer', :member => @member)
+    @profile = FactoryGirl.create(:profile, :member => @member)
   end
 
   it "infers the controller path" do
@@ -16,8 +16,6 @@ describe "sales/index.html.erb" do
   end
 
   xit "should display the home page" do
-    @projects = double(Project).as_null_object
-    @profiles = double(Profile).as_null_object
     render
     expect(rendered).to match(/Talk is cheap/)
     expect(rendered).not_to match(/Getta buncha/)
@@ -33,39 +31,3 @@ describe "sales/index.html.erb" do
     #view.content_for(:sidebar).should have_selector('div.quote')
   end
 end
-
-=begin
-Moved here to keep track of things as pending and errors
-get worked through.
-
-From  http://rubyforge.org/pipermail/rspec-users/2011-July/020315.html
-
-You _can_ use shared content, but what you have here ^^ doesn't work as
-written. The scope in the shared_examples_for block is an example group scope,
-not an example scope, so methods like fill_in and click_button are not
-available. You'd want to wrap them in a before block. Also,
-page.has_content?(...) will return true or false, but will not act like an
-expectation (i.e. RSpec won't care which it returns).
-
-Here are a couple of ways you could do this that will work as Chris expects:
-
-shared_context "logged in" do
- let(:user) { Factory(:user) }
- before do
-   fill_in :login, :with => user.email
-   fill_in :password, :with => user.password
-   click_button "login_button"
- end
-end
-
-describe ArticlesController do
- include_context "logged in"
- describe "GET edit" do
-   it "does something" do
-     # here the shared before hook has already run
-     # and you have access to the `user` generated
-     # by the `let` declaration.
-   end
- end
-end
-=end
