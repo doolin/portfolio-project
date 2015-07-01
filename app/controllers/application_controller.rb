a25f591155096aeb1f11df2e443beefe2ea91ba8
Dave Doolin <david.doolin@gmail.com>
2015-07-01 05:43:42 -0700

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   protected
 #=begin
-def after_sign_in_path_for(resource)
+def after_sign_in_path_for(_resource)
   #stored_location_for(resource) || member_path
   #member_path
 end
