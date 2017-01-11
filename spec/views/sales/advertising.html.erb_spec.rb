cd8e0e3e3204ba6fab5187c7ca78eda1fc69c5ff
Dave Doolin <david.doolin@gmail.com>
2017-01-11 06:17:59 -0800

    starting to get a grip on view spec stubbing

--- spec/views/sales/advertising.html.erb_spec.rb
+++ spec/views/sales/advertising.html.erb_spec.rb
@@ -9,16 +9,19 @@ describe 'sales/advertising' do
     expect(controller.request.path_parameters[:action]).to eq('advertising')
   end
 
-  xit 'should display advertising solicitation' do
+  it 'should display advertising solicitation' do
+    allow(view).to receive(:title).and_return('')
     render
     expect(rendered).to match(/Buy Advertising on Portfolio Project/)
   end
 
   xit 'should have the correct <title> element ' do
+    allow(view).to receive(:title).and_return('')
     render(template: 'sales/advertising', layout: 'layouts/application')
     # have_selector is from webrat
     # https://github.com/brynary/webrat/blob/master/lib/webrat/core/matchers/have_selector.rb
-    expect(rendered).to have_selector('title', text: 'Advertising | Portfolio Project', visible: false)
+    # expect(rendered).to have_selector('title', text: 'Advertising | Portfolio Project', visible: false)
+    expect(rendered).to have_selector('title', text: "\nYour Accomplishmentss\n| Portfolio Project\n", visible: false)
     # http://blog.carbonfive.com/2011/03/02/a-look-at-specifying-views-in-rspec/
     # view.content_for(:sidebar).should have_selector('div.quote')
   end
