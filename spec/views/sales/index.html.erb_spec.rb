bfceef81eb53f8da529b86ec8f62b84c4e75f513
Dave Doolin <david.doolin@gmail.com>
2017-01-14 09:39:39 -0800

    render the front page of the site
    This was a complicated spec to fix. I worked on it over
    the course of several days to untangle all the method calls.
    (A few minutes at a time, not whole days.)
    
    The take-home here is severalfold. First, excessive mocking
    is definitely a smell indicating the code being tested is
    poorly structured. Second, rspec mocks and stubs have changed
    a fair bit between rspec 2 and rspec 3. Not being a big fan
    of mocking in any case, it was annoying to deal with the
    differences. However, my mocking-fu is improved as a result of
    getting this spec working, so that's not entirely a bad thing.
    
    Going forward from this commit, several things need to happen.
    The spec files need to be renamed from erb to haml. The view needs
    to be refactored in favor of using helpers which are much easier
    to test, and the current set of mocks in the spec will get
    reduced along the way.
    
    It's interesting to see my Rails code from ~2010. Pretty terrible
    stuff!

--- spec/views/sales/index.html.erb_spec.rb
+++ spec/views/sales/index.html.erb_spec.rb
@@ -22,17 +22,34 @@ describe 'sales/index' do
 
     # allow(controller).to receive(:title).and_return('123')
 
-    view.stub(:title).and_return('foo')
-    view.stub(:devise_error_messages!).and_return([])
+    member = Member.new id: 1
+
+    allow(view).to receive(:title).and_return('foo')
+    allow(view).to receive(:devise_error_messages!).and_return([])
+
+    ### This one next...
+    # allow(view).to receive(:link_to).with(anything).and_return(nil)
+
+    allow(view).to receive(:gravatar_for).with(member, {:size=>"200"}).and_return('foo')
+    # allow(view).to receive(:gravatar_for).with(anything).and_return('foo')
+
     # view.stub(:profiles).and_return([])
-    profile = Profile.new id: 1
-    expect(Profile).to receive(:find).with(anything).and_return(profile)
-    expect(Member).to receive(:find).with(nil).and_return(nil)
+    profile = Profile.new id: 1, member_id: 1, created_at: Time.now.utc
 
+    # allow(view).to receive(:pluralize).with(anything).and_return('foo')
+    allow(view).to receive(:pluralize).with(0, 'project').and_return('foo')
 
+    # expect(Profile).to receive(:find).with(anything).and_return(profile)
+    expect(Profile).to receive(:find).with(anything).and_return(profile).exactly(4).times
+
+    # expect(Member).to receive(:find).with(nil).and_return(Member.new)
+    expect(Member).to receive(:find).with(member.id).and_return(member).exactly(8).times
+
+    allow(member).to receive(:projects).and_return(projects = [Project.new])
+    allow(projects).to receive(:count).and_return(0)
 
     render
-    ap rendered.class
+    # ap rendered.class
     expect(rendered).to match(/Talk is cheap/)
     expect(rendered).not_to match(/Getta buncha/)
   end
