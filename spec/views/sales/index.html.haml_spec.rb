ca2c601d4cd587c8e61f51ba5380e1a15bec8505
Dave Doolin <david.doolin@gmail.com>
2017-01-14 10:45:31 -0800

    massive example cleanup in sales/index spec

--- spec/views/sales/index.html.haml_spec.rb
+++ spec/views/sales/index.html.haml_spec.rb
@@ -15,41 +15,22 @@ describe 'sales/index' do
     expect(controller.request.path_parameters[:action]).to eq('index')
   end
 
-  # View specs don't load the controller so the helpers aren't loaded
-  # either, hence this fails on no method error for title.
-  # let(:title) { double String }
   it 'should display the home page' do
-
-    # allow(controller).to receive(:title).and_return('123')
-
     member = Member.new id: 1
+    profile = Profile.new id: 1, member_id: 1, created_at: Time.now.utc
 
     allow(view).to receive(:title).and_return('foo')
     allow(view).to receive(:devise_error_messages!).and_return([])
-
-    ### This one next...
-    # allow(view).to receive(:link_to).with(anything).and_return(nil)
-
     allow(view).to receive(:gravatar_for).with(member, {:size=>"200"}).and_return('foo')
-    # allow(view).to receive(:gravatar_for).with(anything).and_return('foo')
-
-    # view.stub(:profiles).and_return([])
-    profile = Profile.new id: 1, member_id: 1, created_at: Time.now.utc
-
-    # allow(view).to receive(:pluralize).with(anything).and_return('foo')
     allow(view).to receive(:pluralize).with(0, 'project').and_return('foo')
-
-    # expect(Profile).to receive(:find).with(anything).and_return(profile)
-    expect(Profile).to receive(:find).with(anything).and_return(profile).exactly(4).times
-
-    # expect(Member).to receive(:find).with(nil).and_return(Member.new)
-    expect(Member).to receive(:find).with(member.id).and_return(member).exactly(8).times
-
     allow(member).to receive(:projects).and_return(projects = [Project.new])
     allow(projects).to receive(:count).and_return(0)
 
+    expect(Profile).to receive(:find).with(anything).and_return(profile).exactly(4).times
+    expect(Member).to receive(:find).with(member.id).and_return(member).exactly(8).times
+
     render
-    # ap rendered.class
+
     expect(rendered).to match(/Talk is cheap/)
     expect(rendered).not_to match(/Getta buncha/)
   end
