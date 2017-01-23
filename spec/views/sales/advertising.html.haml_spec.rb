dc4fe2bc3a936444057b28fc1b3824d031f039dd
Dave Doolin <david.doolin@gmail.com>
2017-01-23 04:30:23 -0800

    reviewing false positive in view spec

--- spec/views/sales/advertising.html.haml_spec.rb
+++ spec/views/sales/advertising.html.haml_spec.rb
@@ -9,20 +9,20 @@ describe 'sales/advertising' do
     expect(controller.request.path_parameters[:action]).to eq('advertising')
   end
 
-  it 'should display advertising solicitation' do
-    allow(view).to receive(:title).and_return('')
-    render
-    expect(rendered).to match(/Buy Advertising on Portfolio Project/)
-  end
+  describe '.rendered' do
+    before do
+      allow(view).to receive(:title).and_return('Premium Membership')
+    end
 
-  xit 'should have the correct <title> element ' do
-    allow(view).to receive(:title).and_return('')
-    render(template: 'sales/advertising', layout: 'layouts/application')
-    # have_selector is from webrat
-    # https://github.com/brynary/webrat/blob/master/lib/webrat/core/matchers/have_selector.rb
-    # expect(rendered).to have_selector('title', text: 'Advertising | Portfolio Project', visible: false)
-    expect(rendered).to have_selector('title', text: "\nYour Accomplishmentss\n| Portfolio Project\n", visible: false)
-    # http://blog.carbonfive.com/2011/03/02/a-look-at-specifying-views-in-rspec/
-    # view.content_for(:sidebar).should have_selector('div.quote')
+    it 'should display advertising solicitation' do
+      render
+      expect(rendered).to match(/Buy Advertising on Portfolio Project/)
+    end
+
+    # This is a false positive
+    xit 'should have the correct <title> element ' do
+      render(template: 'sales/advertising', layout: 'layouts/application')
+      expect(rendered).to match('Premium Membership | Portfolio Project')
+    end
   end
 end
--- spec/views/sales/new.html.haml_spec.rb
+++ spec/views/sales/new.html.haml_spec.rb
@@ -9,16 +9,20 @@ describe 'sales/new' do
     expect(controller.request.path_parameters[:action]).to eq('new')
   end
 
-  it 'should display default scaffolding text' do
-    allow(view).to receive(:title).and_return('')
-    render
-    expect(rendered).to match(/Sales#new/)
-  end
+  describe '.rendered' do
+    before do
+      allow(view).to receive(:title).and_return('')
+    end
 
-  # This passes but it shouldn't.
-  xit 'should have the correct <title> element ' do
-    allow(view).to receive(:title).and_return('')
-    render(template: 'sales/new', layout: 'layouts/application')
-    expect(rendered).to match('Premium Membership | Portfolio Project')
+    it 'displays default scaffolding text' do
+      render
+      expect(rendered).to match(/Sales#new/)
+    end
+
+    # This passes but it shouldn't, false positive.
+    xit 'has the correct <title> element ' do
+      render(template: 'sales/new', layout: 'layouts/application')
+      expect(rendered).to match('Premium Membership | Portfolio Project')
+    end
   end
 end
