6f6f7f3a51e78917a2f90d755803f38222335192
Dave Doolin <david.doolin@gmail.com>
2017-03-03 07:16:32 -0800

    members controller spec cleanup

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -2,29 +2,26 @@
 require 'spec_helper'
 
 describe MembersController do
-  before(:each) do
-    @member = FactoryGirl.create :member
-  end
+  let(:member) { create :member }
 
   describe "GET 'show'" do
     it "should show the member's projects'" do
-      p1 = FactoryGirl.create(:project, member: @member, summary: 'Foo bar')
-      p2 = FactoryGirl.create(:project, member: @member, summary: 'Baz quux')
-      @member.projects = [p1, p2]
-      get :show, params: { id: @member }
+      create :project, member: member, summary: 'Foo bar'
+      create :project, member: member, summary: 'Baz quux'
+      get :show, params: { id: member }
       expect(response).to render_template('show')
     end
   end
 
   describe "GET 'index'" do
     it 'signed in should list member profiles' do
-      sign_in @member
+      sign_in member
       get :index
       expect(response).to redirect_to(profiles_path)
     end
 
     it 'signed out should redirect to sign in path' do
-      sign_out @member
+      sign_out member
       get :index
       expect(response).to redirect_to(new_member_session_path)
     end
