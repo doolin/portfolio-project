f44763d1f2313609b76ee6d1de3c267177792ee2
Dave Doolin <david.doolin@gmail.com>
2017-06-17 08:37:10 -0700

    cucumber: project features now working
    At least, the features are passing, I didn't check
    to see if any assertions were in place and firing
    correctly, which I'm listing as technical debt
    on wush.

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -1,8 +1,23 @@
 
 
 Given /^reader is not logged in$/ do
-  # step %(member name is "foo" with email "foo@test.com" and password "please")
-  visit('/members/sign_out')
+  @member = FactoryGirl.create :member
+  profile = FactoryGirl.create :profile, member_id: @member.id
+
+  featured_profiles = [
+    Profile.find(profile.id),
+    Profile.find(profile.id),
+    Profile.find(profile.id),
+    Profile.find(profile.id)
+  ]
+
+  sales_controller = instance_double("SalesController")
+
+  expect(Profile).to receive(:find).with(anything).and_return(profile).exactly(4).times
+
+  step %(member name is "foo" with email "foo@test.com" and password "please")
+  # visit('/members/sign_out')
+  visit root_path
 end
 
 Given /^is on the project page$/ do
@@ -20,7 +35,8 @@ end
 Given /^is on the member's show page$/ do
   # puts member_root_path
   # visit member_root_path # This is namespaced in routes.rb
-  visit member_path
+  visit member_path(@member)
+  # save_and_open_page
 end
 
 When /^the member clicks "([^"]*)"$/ do |link|
