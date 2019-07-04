6412e2a791b2a3b0a1e1df60622330364aacbc1c
Dave Doolin <david.doolin@gmail.com>
2019-07-04 06:55:42 -0700

    update to ruby 2.6.3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20111117180143) do
+ActiveRecord::Schema.define(version: 2011_11_17_180143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
